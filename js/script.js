4b98dc3bf7025cc6e714cf87f5134a1d36845276
madhieulok <madhieulok2022@gmail.com>
2025-08-25 14:38:18 +0300

    Added my cv and quiz_app

--- js/script.js
+++ js/script.js
@@ -44,14 +44,26 @@ window.onscroll = () => {
 
 /*==== scroll reveal ====*/
 
-/*ScrollReveal({ 
-    reset: true,
-    distance: '80px'
+ScrollReveal({ 
+    //reset: true,
+    distance: '80px',
+    duration: 2000,
     delay: 200
 
 
 });
 
-ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home img, .services-container, .porfolio-box, .contact form', { origin:
-'bottom' }); */
+ScrollReveal().reveal('.home-content, .header', { origin: 'top' });
+ScrollReveal().reveal('.home img, .services, .about, .heading, .skills, .porfolio-container, .contact, .contact form', { origin:
+'bottom' }); 
+ScrollReveal().reveal('.home-content h1, .about-img', { origin:'right' }); 
+ScrollReveal().reveal('.home-content p, .about-content', { origin:'right' }); 
+
+/*==== typed js ====*/
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer','Blogger', 'Web Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
